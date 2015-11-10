96b268abe5d5db32b3373791ea1cfed947326f5f
Zac <zbaston@gmail.com>
2015-11-10 15:22:32 +0000

    create Store controller, create routes/views

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,7 @@
+class StoreController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 
+  root to: 'store#home'
+
+  get 'about', to:'store/about'
+  get 'contact', to: 'store#contact'
+
 end
