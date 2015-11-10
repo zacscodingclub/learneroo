e242647938d49fd90d9975374ccf494a48525e73
Zac <zbaston@gmail.com>
2015-11-10 15:12:00 +0000

    first commit, workspace setup

---
+++ app/models/product.rb
@@ -0,0 +1,27 @@
+class Product < ActiveRecord::Base
+  validates :name, presence: true
+  belongs_to :category
+  
+  def purchase
+    if quantity > 0
+      decrement(:quantity)
+      return true
+    end
+  end
+  
+  def self.available
+    Product.where.not(quantity: 0)
+  end
+  
+  def self.newest
+    Product.order(:created_at).last
+  end
+  
+  def self.oldest
+    Product.order(:created_at).first
+  end
+  
+  def sold_out
+    Product.where(quantity: 0)
+  end
+end
